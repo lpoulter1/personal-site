921e40c586579ba5d1baaf80fcb3f7405e5e9f21
lpoultersky <lpoultersky@gmail.com>
2023-04-07 10:34:24 +0100

    improve responsive

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,11 @@
 <main class="fixed inset-0 bg-black text-white">
   <div class="container mx-auto h-screen">
     <div class="flex justify-center items-center h-full">
-      <div class="flex gap-4 items-center">
+      <div class="flex flex-col sm:flex-row gap-4 items-center">
         <div>
           <img src={Logo} class="h-60 w-auto" alt="Laurie Poulter" />
         </div>
-        <div class="text-2xl leading-relaxed">
+        <div class="text-2xl leading-relaxed w-60 sm:w-auto">
           <p class="mb-4">Hello !</p>
           <p>I'm Laurie.</p>
           <p class="mb-4">I'm a freelance Web Developer.</p>
