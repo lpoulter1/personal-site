336651afe9d8018f61d90e201bdad365e5886a42
lpoultersky <lpoultersky@gmail.com>
2023-04-07 10:29:05 +0100

    first draft of page

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,21 @@
-<main class="fixed inset-0 bg-amber-300">
+<script lang="ts">
+  import Logo from "./assets/logo.jpg";
+</script>
+
+<main class="fixed inset-0 bg-black text-white">
   <div class="container mx-auto h-screen">
-    <div class="flex justify-center items-center bg-amber-200 h-full">
-      <h1 class="text-6xl text-white">Laurie Poulter</h1>
+    <div class="flex justify-center items-center h-full">
+      <div class="flex gap-4 items-center">
+        <div>
+          <img src={Logo} class="h-60 w-auto" alt="Laurie Poulter" />
+        </div>
+        <div class="text-2xl leading-relaxed">
+          <p class="mb-4">Hello !</p>
+          <p>I'm Laurie.</p>
+          <p class="mb-4">I'm a freelance Web Developer.</p>
+          <a href="mailto:lpoulter1984@gmail.com">Contact Me.</a>
+        </div>
+      </div>
     </div>
   </div>
 </main>
